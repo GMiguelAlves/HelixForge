62ac338540782e6eadb0b5583340213da36b743f
GMiguelAlves <g236875@dac.unicamp.br>
2026-08-13 10:46:42 -0300

    Use ChIP Python environment

--- tests/slurm/run_chipseq_production_real.sh
+++ tests/slurm/run_chipseq_production_real.sh
@@ -23,7 +23,7 @@ work_root="${validation_root}/work/${case_name}"
 cache_root="${validation_root}/cache/${case_name}"
 compat_bin="${validation_root}/runtime/bowtie2-direct"
 conda_root=$(cd "$(dirname "$conda_bin")/.." && pwd)
-runtime_path="${compat_bin}:${conda_root}/envs/${r_env}/bin:${conda_root}/envs/${chip_env}/bin:${conda_root}/envs/${rna_env}/bin:${conda_root}/envs/${python_env}/bin:/usr/bin:/bin"
+runtime_path="${compat_bin}:${conda_root}/envs/${python_env}/bin:${conda_root}/envs/${r_env}/bin:${conda_root}/envs/${chip_env}/bin:${conda_root}/envs/${rna_env}/bin:/usr/bin:/bin"
 nextflow_jar=${HELIXFORGE_NEXTFLOW_JAR:-/home/ra236875@bio.ib.unicamp.br/helixforge-validation-20260811/.validation-runtimes/nxf-home-25.10.7/framework/25.10.7/nextflow-25.10.7-one.jar}
 
 case "$validation_root" in
@@ -50,7 +50,8 @@ if [[ "$mode" == "preflight-job" ]]; then
     fastqc --version
     multiqc --version
     Rscript -e 'stopifnot(requireNamespace("DESeq2", quietly=TRUE)); cat("DESeq2 ", as.character(packageVersion("DESeq2")), "\n", sep="")'
-    "${conda_root}/envs/${python_env}/bin/python3" -c 'import pyBigWig; print("pyBigWig", pyBigWig.__version__)'
+    printf 'python3=%s\n' "$(command -v python3)"
+    python3 -c 'import pyBigWig; print("pyBigWig", pyBigWig.__version__)'
     ps --version | head -n 1
     exit 0
 fi
